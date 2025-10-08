d1ffe0db4a56cb091ff45f8bac46c11000497ab5
Abhinav Goel <abhinavgo1000@gmail.com>
2025-10-08 16:10:36 +0530

    app registered and model form in admin fixed

--- med_portal/settings.py
+++ med_portal/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "med_portal_backend"
 ]
 
 MIDDLEWARE = [
--- med_portal_backend/admin.py
+++ med_portal_backend/admin.py
@@ -6,11 +6,12 @@ from med_portal_backend.models import Patient
 class PatientForm(forms.ModelForm):
     class Meta:
         model = Patient
+        fields = "__all__"
 
 
 class MedPortalAdmin(admin.ModelAdmin):
     search_fields = ['first_name', 'last_name', 'email']
-    list_display = ['first_name', 'last_name', 'email']
+    list_display = ['first_name', 'last_name', 'dob', 'email']
     form = PatientForm
 
 
