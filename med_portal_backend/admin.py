31b014df3defbb0384dfa0032a0ddaf401b79404
Abhinav Goel <abhinavgo1000@gmail.com>
2025-10-08 15:31:26 +0530

    admin site configured

--- med_portal_backend/admin.py
+++ med_portal_backend/admin.py
@@ -1,3 +1,14 @@
+from django import forms
 from django.contrib import admin
+from med_portal_backend.models import Patient
 
-# Register your models here.
+class PatientForm(forms.ModelForm):
+    class Meta:
+        model = Patient
+
+class MedPortalAdmin(admin.ModelAdmin):
+    search_fields = ['first_name', 'last_name', 'email']
+    list_display = ['first_name', 'last_name', 'email']
+    form = PatientForm
+
+admin.site.register(Patient, MedPortalAdmin)
--- med_portal_backend/models.py
+++ med_portal_backend/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    dob = models.DateField()
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
