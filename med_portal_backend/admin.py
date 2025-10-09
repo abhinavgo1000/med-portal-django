a19983dd965d60f85aaee21c5b1920d104fa3ec9
Abhinav Goel <abhinavgo1000@gmail.com>
2025-10-09 17:50:07 +0530

    schemas fixed for the app

--- med_portal_backend/admin.py
+++ med_portal_backend/admin.py
@@ -1,11 +1,23 @@
 from django import forms
 from django.contrib import admin
-from med_portal_backend.models import Patient, Medication
+from med_portal_backend.models import Physician, Patient, Medication, Diagnosis
+
+
+class PhysicianForm(forms.ModelForm):
+    class Meta:
+        model = Physician
+        fields = '__all__'
 
 
 class PatientForm(forms.ModelForm):
     class Meta:
         model = Patient
+        fields = "__all__"
+
+
+class DiagnosisForm(forms.ModelForm):
+    class Meta:
+        model = Diagnosis
         fields = "__all__"
 
 
@@ -15,10 +27,23 @@ class MedicationForm(forms.ModelForm):
         fields = "__all__"
 
 
+class PhysicianAdmin(admin.ModelAdmin):
+    list_filter = ['first_name', 'specialty']
+    search_fields = ['first_name', 'last_name']
+    list_display = ['first_name', 'last_name']
+    form = PhysicianForm
+
+
 class PatientAdmin(admin.ModelAdmin):
+    list_filter = ['first_name']
     search_fields = ['first_name', 'last_name', 'email']
     list_display = ['first_name', 'last_name', 'dob', 'email']
     form = PatientForm
+
+
+class DiagnosisAdmin(admin.ModelAdmin):
+    list_filter = ['patient']
+    search_fields = ['diagnosis_name']
 
 
 class MedicationAdmin(admin.ModelAdmin):
@@ -28,5 +53,7 @@ class MedicationAdmin(admin.ModelAdmin):
     form = MedicationForm
 
 
+admin.site.register(Physician, PhysicianAdmin)
 admin.site.register(Patient, PatientAdmin)
+admin.site.register(Diagnosis, DiagnosisAdmin)
 admin.site.register(Medication, MedicationAdmin)
--- med_portal_backend/models.py
+++ med_portal_backend/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 
 
+class Physician(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    dob = models.DateField()
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=100)
+    specialty = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+
 class Patient(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -11,9 +27,22 @@ class Patient(models.Model):
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     zip_code = models.CharField(max_length=100)
+    physician = models.ForeignKey(Physician, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
+
+
+class Diagnosis(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    diagnosis_name = models.CharField(max_length=100)
+    diagnosis_description = models.TextField()
+
+    class Meta:
+        verbose_name_plural = "Diagnoses"
+
+    def __str__(self):
+        return self.diagnosis_name
 
 
 class Medication(models.Model):
